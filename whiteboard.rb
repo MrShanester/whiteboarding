f88b0bfe1be5a02e36541a8c89118a4280956fc5
Shane Nicholson <shanescottnicholson@gmail.com>
2022-04-29 09:49:42 -0700

    Adds volume of a height array problem

--- whiteboard.rb
+++ whiteboard.rb
@@ -27,17 +27,47 @@
 
 #Psuedo linked list reverse number adder
 
-def linked(list_one, list_two)
-  number_one = ""
-  number_two = ""
-  list_one.length.times do
-    number_one += list_one.pop.to_s
-  end
-  list_two.length.times do
-    number_two += list_two.pop.to_s
-  end
+# def linked(list_one, list_two)
+#   number_one = ""
+#   number_two = ""
+#   list_one.length.times do
+#     number_one += list_one.pop.to_s
+#   end
+#   list_two.length.times do
+#     number_two += list_two.pop.to_s
+#   end
 
-  return number_one.to_i + number_two.to_i
+#   return number_one.to_i + number_two.to_i
+# end
+
+# print linked([2,3,4], [5,6,7])
+
+# Maximum volume of  water based on heght and width
+# input: height = [1,8,6,2,5,4,8,3,7]
+# expected output: volume = 49
+
+def volume(height)
+  j = 0
+  i = 0
+  liquid = 0
+  height.length.times do 
+    distance = 1
+    while j < height.length
+      if height[i] > height[j]
+        temp =  height[j]
+      else
+        temp = height[i]
+      end
+      temp = temp * distance
+      liquid = temp if temp > liquid
+      distance += 1
+      j += 1
+      temp = 0
+    end
+    i += 1
+    j = i + 1
+  end
+  return liquid
 end
 
-print linked([2,3,4], [5,6,7])
+print volume([1,8,6,2,5,4,8,3,7])
